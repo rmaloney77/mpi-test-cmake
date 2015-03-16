21c88c88b5a30221ded3658cec46ccd94db5a6ac
RJM <ryan@errantgames.com>
2015-03-16 02:06:15 -0400

    updated main with slightly more interesting example.

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <mpi.h>
 
-static int numprocs;
 
 int main(int argc, char **argv) { 
-	int my_rank; 
-	
-	// MPI initializations 
-	MPI_Status status; 
-	MPI_Init(&argc, &argv); 
-	MPI_Comm_size(MPI_COMM_WORLD, &numprocs); 
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
-	
+	int process_rank;
+	int world_process_count;
+
+	MPI_Status status;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &world_process_count);
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	MPI_Get_processor_name(processor_name, &name_len);
+
 	double time_start = MPI_Wtime();
+
+	float check = process_rank + 1;
+
+	while((MPI_Wtime() - time_start) < check);
+
+	printf("Hello World from processor %s rank %d (of %d). Elapsed time is %f.\n",
+			processor_name,
+			process_rank,
+			world_process_count,
+			(MPI_Wtime() - time_start));
 	
-	printf("Hello World, my rank is %d and the time is %d", my_rank, (MPI_Wtime() - time_start)); 
-	
-	// End MPI 
-	MPI_Finalize(); 
+	MPI_Finalize();
 }
